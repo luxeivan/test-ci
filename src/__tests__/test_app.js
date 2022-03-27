4a39d0153b0053f34de70f99eb17e3c6cf486f32
luxeivan <luxeivan@gmail.com>
2022-03-27 17:15:49 +0300

    first commit

--- src/__tests__/test_app.js
+++ src/__tests__/test_app.js
@@ -1,12 +1,13 @@
 import appObject from '../app';
 
 test.each([
-    ['healthy', {name: 'Маг', health: 90}, 'healthy'], 
-    ['wounded', {name: 'Волшебник', health: 40}, 'wounded'], 
-    ['critical', {name: 'Демон', health: 10}, 'critical']
-])
-    (('should calculate cashback for %s level with %i amount'), 
-    (level, amount, expected) => { 
-        const result = appObject(amount);
-        expect(result).toBe(expected); 
-    });
+  ['healthy', { name: 'Маг', health: 90 }, 'healthy'],
+  ['wounded', { name: 'Волшебник', health: 40 }, 'wounded'],
+  ['critical', { name: 'Демон', health: 10 }, 'critical'],
+])(
+  ('should calculate cashback for %s level with %i amount'),
+  (level, amount, expected) => {
+    const result = appObject(amount);
+    expect(result).toBe(expected);
+  },
+);
--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 export default function appObject(gamer) {
-    if (gamer.health > 50) {
-        return 'healthy';
-    }
-    if (gamer.health > 15) {
-        return 'wounded';
-    }
-    if (gamer.health <= 15) {
-        return 'critical';
-    }
+  if (gamer.health > 50) {
+    return 'healthy';
+  }
+  if (gamer.health > 15) {
+    return 'wounded';
+  }
+  if (gamer.health <= 15) {
+    return 'critical';
+  }
+  return null;
 }
