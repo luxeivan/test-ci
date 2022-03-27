eb2c847a8bf6d7bd76d85f19af589e54bf98a458
luxeivan <luxeivan@gmail.com>
2022-03-27 17:10:54 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+export default function appObject(gamer) {
+    if (gamer.health > 50) {
+        return 'healthy';
+    }
+    if (gamer.health > 15) {
+        return 'wounded';
+    }
+    if (gamer.health <= 15) {
+        return 'critical';
+    }
+}
